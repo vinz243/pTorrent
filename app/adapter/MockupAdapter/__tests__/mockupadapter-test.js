d32e73b1c66aa9b0cf52a71479821f00025090e4
vinz243 <vinz243@gmail.com>
2015-12-20 16:45:34 +0100

    generate skeleton

---
+++ app/adapter/MockupAdapter/__tests__/mockupadapter-test.js
@@ -0,0 +1,14 @@
+// import Client from '../app/adapter';
+// import TorrentIndex from '../app/adapter/MockupAdaper/torrent';
+
+// var client = new Client();
+// client.addTorrent('6A20D919EF6203F8C0CC75D194674605A4B768F0').then((data) => {
+// 	console.log(data);
+// });
+
+describe('TorrentIndex', () =>{
+	it('should declare a `register` function', () => {
+		expect('foo').toBe('foo');
+		// expect(typeof TorrentIndex.register).toBe('function');
+	})
+})
