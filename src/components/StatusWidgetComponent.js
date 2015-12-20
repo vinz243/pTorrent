96ef3bbb7f44ebbe6270670aa47c53cdda79f705
vinz243 <vinz243@gmail.com>
2015-12-20 19:50:35 +0100

    update statuswidget

--- src/components/StatusWidgetComponent.js
+++ src/components/StatusWidgetComponent.js
@@ -2,14 +2,37 @@
 
 import React from 'react';
 
-require('styles//StatusWidget.scss');
+import {Avatar, FontIcon, CardHeader} from 'material-ui/lib';
+import Colors from 'material-ui/lib/styles/colors';
+
+import style from 'styles//StatusWidget.scss';
 
 class StatusWidgetComponent extends React.Component {
   render() {
     return (
-      <div className="statuswidget-component">
-        Please edit src/components///StatusWidgetComponent.js to update this component!
-      </div>
+      <div className={style.statusWidget}>
+
+  			<CardHeader
+  			  title="&#8595; 5.2 Mb/s" titleStyle={{
+  			  								fontSize: "19px",
+  			  								color: "#fff"
+  			  							}}
+  			  subtitle="16 Mb/s &#8593; " subtitleStyle={{
+  			  												color: "#fff",
+  			  												marginTop: "8px"
+  			  											}}
+  			  avatar={
+						<Avatar
+							size={64} icon={
+			  					<FontIcon style={{
+			  						fontSize: '34px',
+			  						margin: '14px'
+			  						}} className="material-icons">cloud_download</FontIcon>
+			  			}
+			  			backgroundColor={Colors.white}
+			  			color={Colors.lime800} />
+  			  }/>
+			</div>
     );
   }
 }
