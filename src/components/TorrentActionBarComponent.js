1c6cfa732f2a57c3918d5eaef0b854f0dcaf0575
vinz243 <vinz243@gmail.com>
2015-12-20 22:27:39 +0100

    add torrent actionbar

---
+++ src/components/TorrentActionBarComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//TorrentActionBar.scss');
+
+class TorrentActionBarComponent extends React.Component {
+  render() {
+    return (
+      <div className="torrentactionbar-component">
+        Please edit src/components///TorrentActionBarComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+TorrentActionBarComponent.displayName = 'TorrentActionBarComponent';
+
+// Uncomment properties you need
+// TorrentActionBarComponent.propTypes = {};
+// TorrentActionBarComponent.defaultProps = {};
+
+export default TorrentActionBarComponent;
