f0b1bb5231e570ddfd2e672260660c2fcc4a96d9
vinz243 <vinz243@gmail.com>
2015-12-20 19:51:08 +0100

    add titlebar

---
+++ src/components/TitleBarComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//TitleBar.scss');
+
+class TitleBarComponent extends React.Component {
+  render() {
+    return (
+      <div className="titlebar-component">
+        Please edit src/components///TitleBarComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+TitleBarComponent.displayName = 'TitleBarComponent';
+
+// Uncomment properties you need
+// TitleBarComponent.propTypes = {};
+// TitleBarComponent.defaultProps = {};
+
+export default TitleBarComponent;
