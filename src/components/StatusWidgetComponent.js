333402de75774c706d4c96dcbdbed1e7a8a35870
vinz243 <vinz243@gmail.com>
2015-12-20 19:48:20 +0100

    add status widget

---
+++ src/components/StatusWidgetComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//StatusWidget.scss');
+
+class StatusWidgetComponent extends React.Component {
+  render() {
+    return (
+      <div className="statuswidget-component">
+        Please edit src/components///StatusWidgetComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+StatusWidgetComponent.displayName = 'StatusWidgetComponent';
+
+// Uncomment properties you need
+// StatusWidgetComponent.propTypes = {};
+// StatusWidgetComponent.defaultProps = {};
+
+export default StatusWidgetComponent;
